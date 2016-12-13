4073233922c517d91b1a5faf3d01a17a97e8e3bc
Sanjay HP <sanjay.p@caritorsolutions.com>
2016-12-13 12:06:21 +0530

    1.ui Keec

--- static/app/main.js
+++ static/app/main.js
@@ -20,6 +20,14 @@ define(['jquery', 'angular', 'angular-ui-router','angular-animate','angular-aria
           .state('app', {
             abstract: true,
             url: '',
+            resolve: {
+              model: function($http, $rootScope){
+                return $http.get('/keec/api/model').then(function(response){
+                  $rootScope.model = response.data;
+                  return response.data;
+                })
+              }
+            },
             views: {
               'header': {templateUrl: '/keec/assets/views/header.html'},
               'footer': {templateUrl: '/keec/assets/views/footer.html'}
@@ -30,7 +38,48 @@ define(['jquery', 'angular', 'angular-ui-router','angular-animate','angular-aria
           .state('app.home', {
             url: '/keec/',
             views: {
-              'content@': {templateUrl: '/keec/assets/views/home.html'}
+              'content@': {
+                templateUrl: '/keec/assets/views/home.html',
+                controller:function($scope,$mdDialog){
+                  $scope.selectedStep=0;
+                  $scope.stepProgress = 1;
+                  $scope.maxStep = 4;
+                   $scope.stepData = [
+                           { step: 1, completed: false,data: {} },
+                           { step: 2, completed: false,data: {} },
+                           { step: 3, completed: false,data: {} },
+                           { step: 4, completed: false,data: {} },
+                       ];
+                  $scope.BuildingInfo={};
+                  $scope.submit=function(stepData){
+                    console.log(stepData);
+                    $scope.enableNextStep = function() {
+               if ($scope.selectedStep >= $scope.maxStep) {
+                 return;
+                }
+        
+        if ($scope.selectedStep === $scope.stepProgress - 1) {
+            $scope.stepProgress = $scope.stepProgress + 1;
+                 }
+           $scope.selectedStep = $scope.selectedStep + 1;
+           stepData.completed=true;
+           console.log( $scope.selectedStep,$scope.stepProgress);
+          }
+                   console.log($scope.BuildingInfo);
+                   $scope.enableNextStep();
+                  }
+                $scope.dialog=function(ev){
+                  $mdDialog.show({
+        controller:  function DialogController($scope, $mdDialog) {
+        
+      },
+      templateUrl: '/keec/assets/views/dialog.html',
+      clickOutsideToClose:true,
+       });
+        }
+                } 
+
+              }
             }
           })
         // If the path doesn't match any of the configured urls redirect to home
