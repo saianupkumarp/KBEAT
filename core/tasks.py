e1640b5ca5efd29a97f0e12049ec910c4d001c30
Anup Kumar <anup.kumar@kapsarc.org>
2017-01-18 18:42:43 +0300

    - Added API to run the Sim Engine - Added Run Task function

--- core/tasks.py
+++ core/tasks.py
@@ -1,3 +1,4 @@
+# coding: utf-8
 from datetime import datetime
 from flask import send_file
 from flask.json import jsonify
@@ -5,7 +6,17 @@ import ujson as json
 from core import data
 from os import path
 import subprocess
+import requests
 import settings
+
+def run_task(args):
+    print "running task"
+    simEngine = "http://10.10.78.17:9001"
+    simHeaders = {'Content-type': 'application/json; charset=UTF-8'}
+    data1 = '{"txtBldgName":"Shetty Bldg","txtBldgAddress":"Tunisia","cmbBldgType":"Hôtel, 3 Etoiles","cmbBldgLocation":"GAFSA","txtBldgNumFloor":1,"txtBldgCondArea":100,"cmbBldgShape":"Hexagon","txtBldgAzi":0,"txtFloorHeight":2,"txtLengX1":10,"txtLengY1":10,"txtLengX2":10,"txtLengY2":10,"txtLengX3":10,"txtLengY3":10,"txtFloorArea":100,"cmbSouthWall":"Mur Pas d’Isolation","cmbNorthWall":"Mur Pas d’Isolation","cmbEastWall":"Mur Pas d’Isolation","cmbWestWall":"Mur Pas d’Isolation","cmbRoof":"Toit Pas d’Isolation","cmbFirstFloorContact":"Sol","txtWinSouthOverhang":0.5,"txtWinSouthFp":0.5,"cmbHotWaterSystem":"Réservoir système DHW au gaz","cmbBldgSystem":"Systeme Monobloc Volume d’Air Variable","txtHeatSetTemp":20,"txtCoolSetTemp":24,"rdbtnWinWwr":true,"southpercent":30,"northpercent":30,"eastpercent":30,"westpercent":30,"glasstype":"Simple Claire","txtSkyltType":"Flat","txtSkyltCvr":13}'
+    result = requests.post(simEngine, data=json.dumps(data1), headers=simHeaders)
+    print result.text
+    # print args
 
 def run_sim():
     BatchPath = str(path.join(settings.DATA_ROOT, "run_ak.bat"))
