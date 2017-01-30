46c1ab1e5a671cbec022d9e8eb279b15c4de6ee6
sanjayhanumegowda <snj.sanju@gmail.com>
2017-01-30 14:50:26 +0530

    1. filtering of the input values and formating of the keys of input values

--- static/app/main.js
+++ static/app/main.js
@@ -256,17 +256,17 @@ function postData(name,data){
       $rootScope.model.steps.forEach(function(obj){
        obj.containers.forEach(function(obj1){
         obj1.parameters.forEach(function(obj2){
-          if(obj2.id !='prev' && obj2.id != 'next' && obj2.id != 'figure' && obj2.id != 'run' && obj2.id != 'display' && obj2.type != 'table' && obj2.id != 'shape'){
-            if(obj2.id == 'lshape' || obj2.id == 'tshape' || obj2.id == 'ushape'){
+          if(obj2.id !='prev' && obj2.id != 'next' && obj2.id != 'window' && obj2.id != 'figure' && obj2.id != 'run' && obj2.id != 'display' && obj2.type != 'table' && obj2.id != 'shape'){
+            /*if(obj2.id == 'lshape' || obj2.id == 'tshape' || obj2.id == 'ushape'){
               if(obj2.value.x1 !=10){
                 resJson[obj2.id] = obj2.value;
                 ObjCount=ObjCount+2;
               }
-            }
-            else{
-               resJson[obj2.id] = obj2.value;
+            }*/
+            
+            resJson[obj2.id] = obj2.value;
             ObjCount=ObjCount+2;
-            }
+            
           }
         });
       });
@@ -303,8 +303,6 @@ function postData(name,data){
     case 'dropdown':
     scope.field.options = scope.field.options.split(', ');
     scope.field.values = scope.field.values.split(', ');
-    scope.field.value = scope.field.values[0];
-
     break;
     case 'radio':
     scope.field.options = scope.field.options.split(', ');
@@ -546,6 +544,7 @@ function postData(name,data){
       scope.runData();
       $rootScope.stepNext();
     }
+    break;
   }
   scope.dialogBox =function(ev){
     $rootScope.Dialog(ev);
